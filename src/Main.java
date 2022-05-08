5776b3efc4b7e768038448b4bd1032358b276688
KsuChe <nerro@mail.ru>
2022-05-08 16:33:28 +0300

    task1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        //task1
+        String firstName = "Ivan ";
+        String middleName = "Ivanovich ";
+        String lastName = "Ivanov ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
+        System.out.println();
 
     }
 }
